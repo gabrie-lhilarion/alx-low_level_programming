f80e0ac97ed0a7ba497647da72f13f96ac4047bd
gabrie-lhilarion <gabrielhilarion@gmail.com>
2023-10-05 13:17:05 +0100

    Correct files

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,8 @@
-
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - Print a string, followed by a new line, to stdout.
+ * @str: Type char pointer
+ * Return: Always 0.
+ */
+void _puts(char *str) return (0)
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-		
+
 /**
  * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
@@ -11,17 +11,14 @@ char *_strcpy(char *dest, char *src)
 	int l = 0;
 	int x = 0;
 
-
 	while (*(src + l) != '\0')
 	{
 		l++;
 	}
-	for ( ; x < l ; x++)
+	for (; x < l; x++)
 	{
 		dest[x] = src[x];
 	}
 	dest[l] = '\0';
 	return (dest);
 }
- 
-
