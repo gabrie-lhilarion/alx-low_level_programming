b1bf7bc6c6cbfcd9c881f2e8c2eb2801bfcdc8d7
gabrie-lhilarion <gabrielhilarion@gmail.com>
2023-10-05 14:13:18 +0100

    Make corections

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
-#include "main.h"
-/**
- * _puts - Print a string, followed by a new line, to stdout.
- * @str: Type char pointer
- * Return: Always 0.
- */
-void _puts(char *str) return (0)
+
