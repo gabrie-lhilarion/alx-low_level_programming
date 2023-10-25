a243abc68febef1b83500f26f1e74a0defa67089
gabrie-lhilarion <gabrielhilarion@gmail.com>
2023-10-25 04:01:41 +0100

    Correct betty warnings and errors

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,34 +10,32 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *temp = *head;
-    listint_t *current_node = NULL;
-    unsigned int i = 0;
+	listint_t *temp = *head;
+	listint_t *current_node = NULL;
+	unsigned int i = 0;
 
-    if (*head == NULL)
-        return (-1);
+	if (*head == NULL)
+		return (-1);
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
 
-        free(temp);
+	while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
 
-        return (1);
-    }
+		temp = temp->next;
+		i++;
+	}
 
-    while (i < index - 1)
-    {
-        if (!temp || !(temp->next))
-            return (-1);
+	current_node = temp->next;
+	temp->next = current_node->next;
+	free(current_node);
 
-        temp = temp->next;
-        i++;
-    }
-
-    current_node = temp->next;
-    temp->next = current_node->next;
-    free(current_node);
-
-    return (1);
+	return (1);
 }
