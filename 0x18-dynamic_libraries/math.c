cfaa4ad0fe6acfe4333207bba88a94112d6532fe
garie-lhilarion <gabrielhilarion@gmail.com>
2023-12-11 21:15:38 -0800

    C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integers.
+ * @a: interger value
+ * @b: integer value.
+ * Return: integer
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * substract - substracts values.
+ * @a: type int argument
+ * @b: type integer.
+ * Return: integer
+ */
+
+int subtract(int a, int b)
+{
+	return (a - b);
+}
