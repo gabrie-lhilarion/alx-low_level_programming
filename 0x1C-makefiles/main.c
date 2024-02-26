9a7d5c193f10fe7bc457be0f3d372fa2aad0628d
garie-lhilarion <gabrielhilarion@gmail.com>
2024-02-26 11:24:44 +0100

    First make file

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "school.h"
+
+int main() {
+    school_function();
+    return 0;
+}
---
+++ 0x1C-makefiles/school.h
@@ -0,0 +1,6 @@
+#ifndef SCHOOL_H
+#define SCHOOL_H
+
+void school_function();
+
+#endif /* SCHOOL_H */
