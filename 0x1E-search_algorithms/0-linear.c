6d0afa4eb2cfce58b89940f63b1524c2b6981da0
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-04-10 06:35:32 +0100

    Correct display error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,13 +19,12 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%ld] = [%ld] \n", (long int)i, (long int)array[i]);
 		if (array[i] == value)
 		{
 			return (i);
 		}
 	}
 
-
 	return (-1);
 }
-
