63fdfc6e47ed9b12283f136fa98040c151288e17
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-04-10 06:08:57 +0100

    Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * linear_search - prints number of arguments passed into it.
+ * @array: array to search
+ * @size: size of the array
+ * @value: value to find
+ * Return: value if found otherwise -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+
+
+	return (-1);
+}
+
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,3 @@
+#ifndef SEARCH_ALGOS_H
+	int linear_search(int *array, size_t size, int value);
+#endif
